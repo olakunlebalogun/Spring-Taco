ed3c2235821b245e32e8848fc0fe16d3e75a431d
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-07-20 13:01:28 +0100

    first commit

---
+++ src/main/java/com/springproject/taco/TacoApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springproject/taco/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.springproject.taco.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home () {
+        return "This is home.html";
+    }
+}
