3d86746dc0a9c985d3b8f48d7e177c7eca51e318
larry659 <larryfunmbi26@gmail.com>
2022-08-03 14:11:34 +0100

    generated the entity classes for the sql_hr db

--- src/main/java/com/springproject/taco/models/Offices.java
+++ src/main/java/com/springproject/taco/models/Offices.java
@@ -1,4 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.springproject.taco.models;
 
-public class Offices {
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author USER
+ */
+@Entity
+@Table(name = "offices")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Offices.findAll", query = "SELECT o FROM Offices o")})
+public class Offices implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "office_id")
+    private Integer officeId;
+    @Basic(optional = false)
+    @Column(name = "address")
+    private String address;
+    @Basic(optional = false)
+    @Column(name = "city")
+    private String city;
+    @Basic(optional = false)
+    @Column(name = "state")
+    private String state;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "officeId")
+    private List<Employees> employeesList;
+
+    public Offices() {
+    }
+
+    public Offices(Integer officeId) {
+        this.officeId = officeId;
+    }
+
+    public Offices(Integer officeId, String address, String city, String state) {
+        this.officeId = officeId;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+    }
+
+    public Integer getOfficeId() {
+        return officeId;
+    }
+
+    public void setOfficeId(Integer officeId) {
+        this.officeId = officeId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @XmlTransient
+    public List<Employees> getEmployeesList() {
+        return employeesList;
+    }
+
+    public void setEmployeesList(List<Employees> employeesList) {
+        this.employeesList = employeesList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (officeId != null ? officeId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Offices)) {
+            return false;
+        }
+        Offices other = (Offices) object;
+        if ((this.officeId == null && other.officeId != null) || (this.officeId != null && !this.officeId.equals(other.officeId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.springproject.taco.models.Offices[ officeId=" + officeId + " ]";
+    }
+    
 }
